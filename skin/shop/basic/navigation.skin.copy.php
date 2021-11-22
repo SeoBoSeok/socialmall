b03dc10c59efba604ee31d5005a3bac751789dd8
SeoBoSeok <ggy7bbo@gmail.com>
2021-11-22 10:38:07 +0900

    hot fix

--- skin/shop/basic/navigation.skin.copy.php
+++ skin/shop/basic/navigation.skin.copy.php
@@ -32,6 +32,38 @@ add_stylesheet('<link rel="stylesheet" href="'.G5_SHOP_CSS_URL.'/style.css">', 0
 add_javascript('<script src="'.G5_JS_URL.'/shop.category.navigation.js"></script>', 10);
 ?>
 
+<div id="sct_location" class="<?php echo implode(' ', $location_class);?>"> <!-- class="view_location" --> <!-- view_location는 리스트 말고 상품보기에서만 표시 -->
+    <a href='<?php echo G5_SHOP_URL; ?>/' class="go_home"><span class="sound_only">메인으로</span><i class="fa fa-home" aria-hidden="true"></i></a>
+    <i class="dividing-line fa fa-angle-right" aria-hidden="true"></i>
+    <?php if ( is_array($navi_datas) && $navi_datas ){ ?>
+
+        <?php if( isset($navi_datas[0]) && $navi_datas[0] ){ ?>
+        <select class="shop_hover_selectbox category1">
+            <?php foreach((array) $navi_datas[0] as $data ){ ?>
+                <option value="<?php echo $data['ca_id']; ?>" data-url="<?php echo $data['url']; ?>" <?php if($ca_ids['ca_id'] === $data['ca_id']) echo 'selected'; ?>><?php echo $data['ca_name']; ?></option>
+            <?php } ?>
+        </select>
+        <?php } ?>
+        <?php if( isset($navi_datas[1]) && $navi_datas[1] ){ ?>
+        <i class="dividing-line fa fa-angle-right" aria-hidden="true"></i>
+        <select class="shop_hover_selectbox category2">
+            <?php foreach((array) $navi_datas[1] as $data ){ ?>
+                <option value="<?php echo $data['ca_id']; ?>" data-url="<?php echo $data['url']; ?>" <?php if($ca_ids['ca_id2'] === $data['ca_id']) echo 'selected'; ?>><?php echo $data['ca_name']; ?></option>
+            <?php } ?>
+        </select>
+        <?php } ?>
+        <?php if( isset($navi_datas[2]) && $navi_datas[2] ){ ?>
+        <i class="dividing-line fa fa-angle-right" aria-hidden="true"></i>
+        <select class="shop_hover_selectbox category3">
+            <?php foreach((array) $navi_datas[2] as $data ){ ?>
+                <option value="<?php echo $data['ca_id']; ?>" data-url="<?php echo $data['url']; ?>" <?php if($ca_ids['ca_id3'] === $data['ca_id']) echo 'selected'; ?>><?php echo $data['ca_name']; ?></option>
+            <?php } ?>
+        </select>
+        <?php } ?>
+    <?php } else { ?>
+        <?php echo get_text($g5['title']); ?>
+    <?php } ?>
+</div>
 <script>
 jQuery(function($){
     $(document).ready(function() {
--- theme/basic/skin/content/introduce/content.skin.php
+++ theme/basic/skin/content/introduce/content.skin.php
@@ -5,14 +5,33 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 add_stylesheet('<link rel="stylesheet" href="'.$content_skin_url.'/style.css">', 0);
 ?>
 <link rel="stylesheet" href="/css/mobile/style.css?ver=191202">
+<div id="main_bn3">
+  <div class="lt_bn">
+      <ul>
+          <li class="list_1">
+              <div class="bg"><img src="/img/general_banner.png?211012"></div>
+          </li>
+
+      </ul>
+  </div>
+  <div id="bx_pager" class="bx_pager">
+      <ul>
+          <!-- <li> <a href="/general_market.php" class="">보은종합시장 소개</a></li> -->
+          <!-- <li> <a href="/bbs/board.php?bo_table=market" class="active">상점소개</a></li> -->
+          <!-- <li> <a href="/bbs/board.php?bo_table=category" class="">카테고리</a></li> -->
+          <!-- <li> <a href="/bbs/board.php?bo_table=market_news" class="">시장소식</a></li> -->
+      </ul>
+  </div>
+</div>
 <div id="container" class="idx-container">
-        <h1 id="container_title"><span>결초보은시장 &amp; 결초보은 조목조목 협동조합</span></h1>
+        <!-- <h1 id="container_title"><span>결초보은시장 &amp; 결초보은 조목조목 협동조합</span></h1> -->
           
+
 
 <!-- 배너 최신글 -->
 
 <!-- 배너 최신글 시작 { -->
-<div class="lt_bn">
+<!-- <div class="lt_bn">
     <ul>
             <li class="list_1">
             <div class="bg"><img src="http://www.boeunmarket.com/data/file/banner/0_B16iD4of_ebd14258f1f200e676630fca236ec4791b44a7a5.png"></div>
@@ -24,7 +43,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$content_skin_url.'/style.css">',
             </div>
         </li>
         </ul>
-</div>
+</div> -->
 
 
 
@@ -75,7 +94,7 @@ $(".sbn_slide").owlCarousel({
  </script>
 <div id="sidx">
     
-    <div class="sct_wrap" style="margin:150px 0 0 0">
+    <!-- <div class="sct_wrap" style="margin:150px 0 0 0">
         <h2>‘대추나무’의 아름다운 변신</h2>
         <p class="company_text">(결초보은 조목조목 협동조합)은<br>
         높은 품질의 장인 정신과 시대를 초월한 심미적 디자인을 지향합니다.<br>
@@ -89,45 +108,27 @@ $(".sbn_slide").owlCarousel({
         <p class="company_text">모든 작품은 장인들의 솜씨로 하나하나 정성을 다해 가장 이상적인 모습으로 만듦니다.<br>
         이러한 작품은 나무의 결, 모양, 색, 질감에 따라 달라지는 매력을 선사합니다.</p>
         <img class="company_image" src="/img/compay_image2.png">
+    </div> -->
+
+<div class="sct_wrap">
+    <div class="since">
+        <h2>Since 2017,</h2>
+        <p>여러분의 많은 관심 부탁드립니다.</p>
+        <div class="sct_img_wrap">
+            <img src="/img/since-1.jpg">
+            <img src="/img/since-2.jpg">
+            <img src="/img/since-3.jpg">
+            <img src="/img/since-4.jpg">
+            <img src="/img/since-5.jpg">
+            <img src="/img/since-6.jpg">
+            <img src="/img/since-7.jpg">
+            <img src="/img/since-8.jpg">
+            <img src="/img/since-9.jpg">
+            <img src="/img/since-10.jpg">
+        </div>
     </div>
-
-    <div class="sct_wrap">
-      <div class="since">
-        <h2>Since 2007,</h2>
-        <p>보은의 대추나무는 오랜 시간동안 많은분들과 사랑받아왔습니다.</p>
-        <img src="/img/since-1.png">
-        <img src="/img/since-2.png">
-        <img src="/img/since-3.png">
-        <img src="/img/since-4.png">
-        <img src="/img/since-5.png">
-        <img src="/img/since-6.png">
-        <img src="/img/since-7.png">
-        <img src="/img/since-8.png">
-        <img src="/img/since-9.png">
-        <img src="/img/since-10.png">
-      </div>
-
-      <ul class="sct smt_20">
-          <li class="sct_li">
-            <div class="li_wr right" style="text-align:right">
-              <div style="margin-top:90px">2020-09-22 (가칭) 보은 대추나무 가공 협동조합 임시총회<div>
-              <div class="text" style="margin-top:60px">2020-10-07 결초보은 조목조목 협동조합 창립총회</div>
-            </div>
-          </div></div></li>
-          <li class="sct_li" style="border-left:1px solid #777">
-            <div class="li_wr" style="text-align:left">
-            <div style="margin-top:50px">2020-09-14 (가칭) 보은 대추나무 가공 협동조합 임시총회 공고</div>
-            <div style="margin-top:60px">2020-09-29 결초보은 조목조목 협동조합 창립총회 공고</div>
-            <div style="margin:60px 0 50px 0">2020-10-27 결초보은 조목조목 협동조합 설립<div>
-            </div>
-          </div></div></li>
-        </ul>
-
-
-
-
-      <img class="" src="/img/compay_image3-1.png">
-    </div>
+    <!-- <img class="" src="/img/compay_image3-1.png"> -->
+</div>
 
 
     <div class="sct_wrap">
